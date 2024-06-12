53445d6313006e1d8579fbc924805ae8ca0b20d6
cuongdn-3440 <dang.nghiep.cuong@sun-asterisk.com>
2024-06-12 17:16:24 +0700

    slider manual, hot deals list

--- js/app.js
+++ js/app.js
@@ -13,4 +13,38 @@ $(document).ready(function () {
         $('#hamburger_menu').css('display', 'none')
         $('.shadow_layer').css('display', 'none')
     })
+
+    $('#slide_navigator').on('click', '#slide_arrow_right', function () {
+        let leftPosition = $('#slides').css('left')
+        let lastIndex = $('#slides').css('left').length - 2
+        let leftPositionNumber = leftPosition.substring(0, lastIndex)
+        const bannerWidth = 640
+        if (leftPositionNumber % bannerWidth != 0) {
+            return
+        }
+        let currentSlide = leftPositionNumber / -bannerWidth
+        let nextSlidePosition = (currentSlide + 1) * (-bannerWidth)
+        if (Math.abs(nextSlidePosition) >= bannerWidth*5) {
+            $('#slides').css('left', '0px')
+            return
+        }
+        $('#slides').css('left', nextSlidePosition + 'px')
+    })
+
+    $('#slide_navigator').on('click', '#slide_arrow_left', function () {
+        let leftPosition = $('#slides').css('left')
+        let lastIndex = $('#slides').css('left').length - 2
+        let leftPositionNumber = leftPosition.substring(0, lastIndex)
+        const bannerWidth = 640
+        if (leftPositionNumber % bannerWidth != 0) {
+            return
+        }
+        let currentSlide = leftPositionNumber / -bannerWidth + 1
+        let nextSlidePosition = (currentSlide - 2) * (-bannerWidth)
+        if (nextSlidePosition > 0) {
+            $('#slides').css('left', -bannerWidth*4 + 'px')
+            return
+        }
+        $('#slides').css('left', nextSlidePosition + 'px')
+    })
 })
